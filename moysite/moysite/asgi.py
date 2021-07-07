14f8749e3bf3d1d2987f9cb2d5ccd2f0ce095f66
Anton B <avhobud@gmail.com>
2021-07-07 16:45:02 +0300

    site

--- moysite/moysite/asgi.py
+++ moysite/moysite/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for four_years project.
+ASGI config for moysite project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'four_years.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'moysite.settings')
 
 application = get_asgi_application()
